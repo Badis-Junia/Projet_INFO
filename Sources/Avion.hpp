8e982da97bdcb15e48f3bed785589f6008d5bf1e
dahi_ <badis.dahi@junia.student.com>
2025-11-12 14:00:15 +0100

    test

---
+++ Sources/Avion.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <thread>
+#include "Agent.hpp"
+
+class Avion : public Agent {
+private:
+    std::string compagnie;
+    double positionX, positionY, altitude;
+    double vitesse;
+    std::string etat;
+    bool urgence;
+
+public:
+    Avion(const std::string& id, const std::string& compagnie);
+    ~Avion();
+    void run() override;
+    void majposition();
+    void atterisage();
+    void decollage();
+    void Urgence(bool etatUrgence);
+    bool estEnUrgence() const;
+
+};
