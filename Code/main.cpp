adceac352806701f480ac3666e731eea1759b880
Badis-Junia <badis.dahi@student.junia.com>
2025-11-17 17:10:55 +0100

    test

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,7 +1,7 @@
 // #include <cstdlib>
 // #include <iostream>
 #include <SFML/Graphics.hpp>
-
+#include <iostream>
 #include "Sources.hpp"
 
 
@@ -18,6 +18,33 @@ int main() {
     sf::RenderWindow app(sf::VideoMode({WINDOW_SIZE.x, WINDOW_SIZE.y}, 32), "Projet_INFO");
 
     app.setFramerateLimit(60); 
+    sf::Texture backgroundImage(path_image + "background.png");
+    sf::Sprite backgroundSprite(backgroundImage);
+
+	if (!backgroundImage.loadFromFile(path_image + "background.png"))
+	{
+        std::cerr << "Erreur pendant le chargement des images" << std::endl;
+		return EXIT_FAILURE; // On ferme le programme
+	}
+
+	backgroundSprite.setTexture(backgroundImage);
+
+
+	while (app.isOpen()) // Boucle principale
+	{
+		while (const std::optional event = app.pollEvent())
+		{
+			if ((event->is<sf::Event::KeyPressed>() && event->getIf<sf::Event::KeyPressed>()->code == sf::Keyboard::Key::Escape) ||
+				event->is<sf::Event::Closed>())
+			{
+				app.close();
+			}
+		}
+		// Affichages
+		app.clear();
+		app.draw(backgroundSprite);
+		app.display();
+	}
 
     return 0;
 }
