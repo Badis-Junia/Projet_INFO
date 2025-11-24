412b644d3bddbfdd6e2873a6d53d247bf282fc89
Badis-Junia <badis.dahi@student.junia.com>
2025-11-24 13:44:20 +0100

    main

--- Code/main.cpp
+++ Code/main.cpp
@@ -17,12 +17,9 @@ int lasimulation() {
 
     sf::Texture backgroundImage, avionTexture, tourControleTexture, aeroportTexture;
 
-    if (!backgroundImage.loadFromFile(path_image + "background.png") ||
-        !avionTexture.loadFromFile(path_image + "avion.png") ||
-        !tourControleTexture.loadFromFile(path_image + "tourcontrole.png") || 
-        !aeroportTexture.loadFromFile(path_image + "aeroport.png")) {
-            std::cerr << "Erreur pendant le chargement des images" << std::endl;
-            return -1;
+    if (!backgroundImage.loadFromFile(path_image + "background.png") || !avionTexture.loadFromFile(path_image + "avion.png") || !tourControleTexture.loadFromFile(path_image + "tourcontrole.png") || !aeroportTexture.loadFromFile(path_image + "aeroport.png")) {
+        std::cerr << "Erreur pendant le chargement des images" << std::endl;
+        return -1;
     }
 
     sf::Sprite backgroundSprite(backgroundImage), avionSprite(avionTexture), tourControleSprite(tourControleTexture), aeroportSprite(aeroportTexture), aeroport2Sprite(aeroportTexture);
@@ -33,14 +30,8 @@ int lasimulation() {
     CentreControleRegional aeroport2("11");
 
     avionSprite.setScale(sf::Vector2f(1, 1));
-
     tourControleSprite.setScale(sf::Vector2f(0.3f, 0.3f));
-    tourControleSprite.setPosition(sf::Vector2f(1000.f, 500.f)); 
-
-
     aeroportSprite.setScale(sf::Vector2f(0.2, 0.2));
-
-
     aeroport2Sprite.setScale(sf::Vector2f(0.2, 0.2));
 
 
@@ -60,23 +51,18 @@ int lasimulation() {
             }
         }
 
+        tourControleSprite.setPosition(sf::Vector2f(1000.f, 500.f)); 
         
+        aeroportSprite.setPosition(sf::Vector2f((100),(200)));
+
+        aeroport2Sprite.setPosition(sf::Vector2f((400),(600)));
+
+
         avionSprite.setPosition(sf::Vector2f(
             static_cast<float>(avionTest.getPositionX() / 10.f), 
             static_cast<float>(avionTest.getPositionY() / 10.f)
         ));
 
-        aeroportSprite.setPosition(sf::Vector2f(
-            static_cast<float>(1000 / 10.f),
-            static_cast<float>(2000 / 10.f)
-
-        ));
-
-        aeroport2Sprite.setPosition(sf::Vector2f(
-            static_cast<float>(aeroport2.getPositionX() / 10.f),
-            static_cast<float>(aeroport2.getPositionY() / 10.f)
-
-        ));
         app.clear();
 
         std::vector<sf::Sprite> tableausprite = {backgroundSprite, tourControleSprite, aeroportSprite, aeroport2Sprite, avionSprite};        
@@ -84,7 +70,6 @@ int lasimulation() {
         for(long unsigned int i = 0;i<tableausprite.size();i++) {
             app.draw(tableausprite[i]);
         }
-
 
         app.display();
     }
@@ -94,6 +79,11 @@ int lasimulation() {
 
     return 0;
 }
+
+
+
+
+
 
 int main() {
     lasimulation();
