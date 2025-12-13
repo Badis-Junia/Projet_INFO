326ae8c5c65fc48efc86bef7658f72a7fb34e910
Badis-Junia <badis.dahi@student.junia.com>
2025-12-14 00:41:40 +0100

    test

--- Code/main.cpp
+++ Code/main.cpp
@@ -6,7 +6,6 @@ int main() {
     try {
         Simulation simulation;
         simulation.executer();
-        std::cout << "simulation lancé " << std::endl;
         return 0;
     } catch (const std::exception& e) {
         std::cerr << "Erreur: " << e.what() << std::endl;
